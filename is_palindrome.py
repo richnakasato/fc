0776092e4211e9a3974a73236d0e5701e7441aa3
Rich Nakasato <richnakasato@hotmail.com>
2018-09-12 12:12:28 -0400

    implemented two pointer method to check palindromicity

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,9 +1,27 @@
 def is_palindrome(input_string):
-    pass
+    #TODO: error handle
+    left = 0
+    right = len(input_string) - 1
+    while left <= right:
+        if input_string[left] != input_string[right]:
+            return False
+        else:
+            left += 1
+            right -= 1
+    return True
 
 
 def main():
-    pass
+    test1 = "madam"
+    test2 = "racecar"
+    test3 = "race car"
+    test4 = ''
+    test5 = 'aabb'
+    print(is_palindrome(test1))
+    print(is_palindrome(test2))
+    print(is_palindrome(test3))
+    print(is_palindrome(test4))
+    print(is_palindrome(test5))
 
 if __name__ == "__main__":
     main()
