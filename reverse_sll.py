ee699175f7d4c583595cb081a606148efa75ac79
Rich Nakasato <richnakasato@hotmail.com>
2018-09-16 21:37:59 -0400

    initial commit for reverse sll and prepend sll

---
+++ reverse_sll.py
@@ -0,0 +1,21 @@
+class SinglyLinkedList:
+    #constructor
+    def __init__(self):
+        self.head = None
+
+    #method for setting the head of the Linked List
+    def setHead(self, head):
+        self.head = head
+
+    def reverse(self):
+        dummy = Node()
+        dummy.setNext(self.head)
+        curr = dummy.getNext()
+        prev = None
+        while curr.next:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        curr.next = prev
+        self.head = curr
