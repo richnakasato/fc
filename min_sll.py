73d9851ec606a3f4bec805f7320b49fd5aa58d08
Rich Nakasato <richnakasato@hotmail.com>
2018-09-30 00:50:41 -0400

    initial commit

---
+++ min_sll.py
@@ -0,0 +1,14 @@
+class BinaryTree:
+    
+    def __init__(self, root_node = None):
+        # Check out Use Me section to find out Node Structure
+        self.root = root_node
+    
+    def find_min(self, root):
+        # Return element should be of type TreeNode
+        if not root:
+            return None
+        elif not root.left_child:
+            return root
+        else:
+            return self.find_min(root.left_child)
