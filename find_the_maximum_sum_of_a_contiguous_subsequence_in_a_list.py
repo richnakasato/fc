e51f65e29eb365a132f6929f242262efd549d764
Rich Nakasato <richnakasato@hotmail.com>
2018-10-15 21:05:17 -0400

    had to look this one up, my implementation was O(n^2)

---
+++ find_the_maximum_sum_of_a_contiguous_subsequence_in_a_list.py
@@ -0,0 +1,21 @@
+import random
+
+# omg kandane! -> definitely did NOT figure this one out!
+def largestContinuousSum(arr):
+    curr_max = glob_max = arr[0]
+    for i in range(1, len(arr)):
+        curr_max = max(arr[i], curr_max+arr[i])
+        glob_max = max(glob_max, curr_max)
+    return glob_max
+
+def main():
+    lo = -10
+    hi = 10
+    n = 10
+    arr = [random.randint(lo, hi) for x in range(n)]
+    print(arr)
+    print(largestContinuousSum(arr))
+
+if __name__ == "__main__":
+    main()
+
