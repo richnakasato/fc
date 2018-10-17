8f2b37af0e44cbd3efda31db9c3f789eec0eaf6d
Rich Nakasato <richnakasato@hotmail.com>
2018-10-17 15:10:42 -0400

    implemented BF version, need to try fast slow ptr method next

--- rearrange_linked_list_in_pairs.py
+++ rearrange_linked_list_in_pairs.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Node():
 
     def __init__(self, data):
@@ -7,36 +9,46 @@ class Node():
 def print_list(head):
     curr = head
     while curr:
-        print('{}->'.format(curr.data), end='')
+        print('{}->'.format(curr.data))
         curr = curr.next
     print('NULL')
 
-def arrange_in_pairs(head):
-    if not head:
-        return
-    else:
-        stack = list()
-        curr = head
-        while curr:
+def get_size(head):
+    count = 0
+    curr = head
+    while curr:
+        count+=1
+        curr = curr.next
+    return count
+
+def arrange_in_pairs_bf(head):
+    size = get_size(head)
+    mid = size//2
+    count = 0
+    stack = list()
+    queue = deque()
+    curr = head
+    while curr:
+        if count < mid:
+            queue.append(curr.data)
+        elif count == mid:
+            queue.append(curr.data)
             stack.append(curr.data)
-            curr = curr.next
-        if len(stack) < 3:
-            return
         else:
-            count = 1
-            last = 1
-            curr = head.next
-            while curr:
-                if count%2:
-                    last = curr.data
-                    curr.data = stack.pop()
-                else:
-                    temp = curr.data
-                    curr.data = last
-                    last = temp
-                count+=1
-                curr = curr.next
-            return
+            stack.append(curr.data)
+        count+=1
+        curr = curr.next
+    print(queue)
+    print(stack)
+    count = 0
+    curr = head
+    while curr:
+        if not count%2:
+            curr.data = queue.popleft()
+        else:
+            curr.data = stack.pop()
+        count+=1
+        curr = curr.next
 
 
 def main():
@@ -49,9 +61,9 @@ def main():
     n1.next = n2
     n2.next = n3
     n3.next = n4
-    n4.next = n5
+    #n4.next = n5
     print_list(head)
-    arrange_in_pairs(head)
+    arrange_in_pairs_bf(head)
     print_list(head)
 
 
