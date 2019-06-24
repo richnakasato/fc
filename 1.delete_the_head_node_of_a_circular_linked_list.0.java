31402f45fa760d864f71c0fd1015e21242dea723
Rich Nakasato <richnakasato@hotmail.com>
2019-06-23 21:17:45 -0400

    implemented

---
+++ 1.delete_the_head_node_of_a_circular_linked_list.0.java
@@ -0,0 +1,9 @@
+// java.util.* and java.util.streams.* have been imported for this problem.
+// You don't need any other imports.
+
+public ListNode deleteAtHead(ListNode head) {
+    if (head == null || head == head.next) return null;
+    head.data = head.next.data;
+    head.next = head.next.next;
+    return head;
+}
