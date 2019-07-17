754422235fbb723bad84b82f21f2cc57a4789ce4
Rich Nakasato <richnakasato@hotmail.com>
2019-07-16 23:40:08 -0400

    implemented, but not tested

---
+++ 2.find_the_transpose_of_a_square_matrix.0.java
@@ -0,0 +1,13 @@
+// java.util.* and java.util.streams.* have been imported for this problem.
+// You don't need any other imports.
+
+public static void transposeMatrix(int[][] matrix) {
+    int count = matrix.length;
+    for (int i=0; i<count; i++) {
+        for (int j=i+1; j<count; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+}
