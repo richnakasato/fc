ad2302fbf2c89d367a8660e12602565c6a2c9130
Rich Nakasato <richnakasato@hotmail.com>
2020-03-22 13:04:24 -0400

    implemented

---
+++ 2.binary_representation.2.java
@@ -0,0 +1,12 @@
+// java.util.* and java.util.streams.* have been imported for this problem.
+// You don't need any other imports.
+
+public static String computeBinary(int val) {
+    if (val == 0) return "0";
+    StringBuilder sb = new StringBuilder();
+    while (val != 0) {
+        sb.append(val % 2);
+        val /= 2;
+    }
+    return sb.reverse().toString();
+}
